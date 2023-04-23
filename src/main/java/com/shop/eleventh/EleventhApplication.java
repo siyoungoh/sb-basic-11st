398ec4166d62cfd85489752d2b5e77742108cdae
siyongoh <dev@syoh.net>
2023-04-24 02:10:43 +0900

    Add Item domain

--- src/main/java/com/shop/eleventh/EleventhApplication.java
+++ src/main/java/com/shop/eleventh/EleventhApplication.java
@@ -3,11 +3,39 @@ package com.shop.eleventh;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.UUID;
+
 @SpringBootApplication
 public class EleventhApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EleventhApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EleventhApplication.class, args);
+    }
 
+}
+
+class Item {
+    private final String id;
+    private String name;
+
+    public Item(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Item(String name) {
+        this(UUID.randomUUID().toString(), name);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
