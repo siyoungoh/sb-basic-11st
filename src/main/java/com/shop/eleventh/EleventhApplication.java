58babec8d75bbad52cacbd87ba304d04d6cde0d0
siyongoh <dev@syoh.net>
2023-04-24 01:36:13 +0900

    Add project skeleton

---
+++ src/main/java/com/shop/eleventh/EleventhApplication.java
@@ -0,0 +1,13 @@
+package com.shop.eleventh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EleventhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EleventhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shop/eleventh/EleventhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.eleventh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EleventhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
